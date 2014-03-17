60d7c8bfeb39b6b19608d1da4e0a380b1879d229
David Brough <david.brough.0416@gmail.com>
2014-03-17 16:48:18 -0400

    MKS_code_tests
    This function has a few tests to make sure that functions MS and MSf are
    working correctly.

---
+++ MKS_code_tests.py
@@ -0,0 +1,27 @@
+import scipy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+from MS import MS
+from MSf import MSf
+
+
+#Tests for MS
+
+Mat2D = sp.rand(93,93)
+MicroS2D = MS(Mat2D,3,1,20,10,1)
+#plt.imshow(MicroS2D)
+#plt.show()
+Mat3D = sp.rand(95,95,95)
+MicroS3D = MS(Mat3D,3,1,20,10,20)
+#plt.imshow(MicroS3D[1,:,:])
+#plt.show()
+
+
+#Tests for MSf
+
+MAT2D = MS(sp.rand(3,3),2,1,1,1,1)
+print MAT2D
+MSf2D = MSf(MAT2D )
+print MSf2D
+MSf3D = MSf(MS(sp.rand(3,3,3),3,1,1,1,1))
+#print MSf3D
