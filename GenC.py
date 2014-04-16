b6c65b3f2cdf52fa53ad2a9064b9ddc9329f233f
Paul Kern <paul.c.kern@gmail.com>
2014-04-16 11:09:33 -0400

    Commented calibration code

--- GenC.py
+++ GenC.py
@@ -2,18 +2,22 @@ from numpy import *
 
 #MicroSF are the Microstructure functions, ABout are the ABAQUS output file names, Macro is the imposed macro strain in the 11 direction
 def GenC(MicroSF_1, MicroSF_2 ABout1, ABout2, Macro)
+	#process ABAQUS output files and return 11 strains for each element
 	strains_1 = ABstrains(ABout1)
 	strains_2 = ABstrains(ABout2)
 	
+	#calculate DFT space responses and microstructure functions
 	response_1_k = np.fft.fftn(strains_1, strains_1.size, [0,1,2])
 	micro_1_k = np.fft.fftn(MicroSF_1, MicroSF_1.size, [0,1,2])
 	
 	response_2_k = np.fft.fftn(strains_2, strains_2.size, [0,1,2])
 	micro_2_k = np.fft.fftn(MicroSF_2, MicroSF_2.size, [0,1,2])
 	
+	#divide responses by inputs to begin calculating coefficients
 	response_1_k[:,:,:] = [x/Macro for x in response_1_k] 
 	response_2_k[:,:,:] = [x/Macro for x in response_2_k] 
 	
+	#explicitly invert matrix and solve for coefficients
 	dim_len = response_1_k.shape[0]
 	coeff = zeros( (dim_len,dim_len,dim_len, 2) )
 	det = zeros( (dim_len,dim_len,dim_len) )
