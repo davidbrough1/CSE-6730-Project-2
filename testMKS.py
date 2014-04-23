bea43c61f1e6794d46f585c6b5630da361f228a6
Paul Kern <paul.c.kern@gmail.com>
2014-04-22 21:05:47 -0400

    added surface plotting
    this appears to work with reasonable accuracy on Paul’s computer for
    stress calculations

--- testMKS.py
+++ testMKS.py
@@ -3,6 +3,8 @@ reload(MKS)
 import MSf as MSf
 import matplotlib.pylab as plt
 import numpy as np
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
 
 #print MKS.GenDelta(5,False)
 
@@ -20,17 +22,17 @@ print 'MSf2,1'
 print MSf2[9:12,9:12,9:12,1]
 '''
 
-stress1 = MKS.ABstrains("deltaM_modeling/21deltaM_1surroundedBy2.dat")
-stress2 = MKS.ABstrains("deltaM_modeling/21deltaM_2surroundedBy1.dat")
-stress41 = MKS.ABstrains("outputModeling/41_2phase.dat")
+stress1 = MKS.ABstrains("deltaM_modeling/21deltaM_1surroundedBy2_strain.dat")
+stress2 = MKS.ABstrains("deltaM_modeling/21deltaM_2surroundedBy1_strain.dat")
+#stress41 = MKS.ABstrains("outputModeling/41_2phase.dat")
 
 """
 Noah's Code Test
 """
-
+"""
 stressNoah1 = stress1.reshape((1,1,21**3)
 stressNoah2 = stress2.reshape((1,1,21**3)
-
+"""
 
 
 
@@ -48,6 +50,34 @@ for ii in range(coeff.shape[3]):
     coeff[:,:,:,ii] = np.roll(coeff[:,:,:,ii], 10, axis = 0)
     coeff[:,:,:,ii] = np.roll(coeff[:,:,:,ii], 10, axis = 1)
     coeff[:,:,:,ii] = np.roll(coeff[:,:,:,ii], 10, axis = 2)
+
+#plot a surface graph of the influence coefficients for the middle slice
+"""
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+Z = coeff[11,:,:,0]
+X = np.zeros((21,21))
+Y = np.zeros((21,21))
+for i in range(21):
+	for j in range(21):
+		X[i,j] = i
+		Y[i,j] = j
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.3,cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='z', offset=-.0005, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='x', offset=0, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='y', offset=0, cmap=cm.coolwarm)
+
+ax.set_xlabel('X')
+ax.set_xlim(0, 21)
+ax.set_ylabel('Y')
+ax.set_ylim(0, 21)
+ax.set_zlabel('Z')
+ax.set_zlim(-.0005, .005)
+
+plt.show()
+"""
+
+
 '''
 for ii in range(2):
     Coef11 = coeff[11,:,:,ii]
@@ -66,10 +96,41 @@ for ii in range(2):
     plt.title('Coef 33 H='+str(ii+1))
     plt.show()
 '''
-MSf41 = MSf.MSf(stress41)
-stressCalc = MKS.NewResponse(fcoeff, .02, MSf2)
+   
+#MSf41 = MSf.MSf(stress41)
+stressCalc = MKS.NewResponse(fcoeff, .02, MSf1)
 stressCalc = np.real_if_close(stressCalc)
 
+#plot a surface graph of the influence coefficients for the middle slice
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+Z = stress1[10,:,:]-stressCalc[10,:,:]
+X = np.zeros((21,21))
+Y = np.zeros((21,21))
+
+Zmin = Z.min()
+Zmax = Z.max()
+for i in range(21):
+	for j in range(21):
+		X[i,j] = i
+		Y[i,j] = j
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.3,cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='z', offset=Zmin, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='x', offset=0, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='y', offset=0, cmap=cm.coolwarm)
+
+ax.set_xlabel('X')
+ax.set_xlim(0, 21)
+ax.set_ylabel('Y')
+ax.set_ylim(0, 21)
+ax.set_zlabel('Z')
+ax.set_zlim(Zmin, Zmax)
+
+plt.show()
+
+
+'''
 plt.subplot(311)
 plt.imshow(stressCalc[8,:,:])
 plt.colorbar()
@@ -84,4 +145,4 @@ plt.subplot(331)
 plt.imshow((stress1[8,:,:]-stressCalc[8,:,:]))
 plt.colorbar()
 plt.show()
-
+'''
