6a3142ae88cc94fc27b71bb03950dc0ee49aca08
Jim Allers <jallers@liveperson.com>
2014-04-24 13:41:19 -0400

    Some more test abaqus stuff

--- TestAbaqusGen.py
+++ TestAbaqusGen.py
@@ -5,15 +5,25 @@ from MS import MS
 from MSf import MSf
 from AbaqusGen import generateAbaqusInp
 from AbaqusGen import getEleNumber
-import pickle
+from AbaqusGen import getIndices
 
-print 'Ele Number for 0, 0, 0: {}'.format(getEleNumber(0, 0, 0, 21, 21, 21, 1))
-print 'Ele Number for 0, 20, 0: {}'.format(getEleNumber(0, 20, 0, 21, 21, 21, 1))
-print 'Ele Number for 1, 0, 0: {}'.format(getEleNumber(1, 0, 0, 21, 21, 21, 1))
-print 'Ele Number for 0, 0, 1: {}'.format(getEleNumber(0, 0, 1, 21, 21, 21, 1))
-print 'Ele Number for 20, 20, 20: {}'.format(getEleNumber(20, 20, 20, 21, 21, 21, 1))
-print 'Ele Number for 11, 11, 11: {}'.format(getEleNumber(11, 11, 11, 21, 21, 21, 1))
-print 'Ele Number for 10, 10, 10: {}'.format(getEleNumber(10, 10, 10, 21, 21, 21, 1))
+def printElementNumber(i, j, k, side_len):
+    print 'Ele Number for {}, {}, {}: {}'.format(i, j, k, getEleNumber(i, j, k, side_len, side_len, side_len, 1))
+
+printElementNumber(0,0,0,21)
+print getIndices(1, 21)
+printElementNumber(0,20,0,21)
+print getIndices(21, 21)
+printElementNumber(0,20,1,21)
+print getIndices(42, 21)
+printElementNumber(1, 0, 0, 21)
+printElementNumber(0, 0, 1, 21)
+printElementNumber(0, 1, 2, 21)
+print getIndices(44, 21)
+printElementNumber(1, 1, 1, 21)
+printElementNumber(20, 20, 20, 21)
+printElementNumber(11, 11, 11, 21)
+printElementNumber(10, 10, 10, 21)
 
 ms = MS(sp.rand(31,31,31),2,1,1,1,1)
 
@@ -23,7 +33,8 @@ generateAbaqusInp('17_2phase.inp', MS(sp.rand(17,17,17),2,1,1,1,1))
 
 #generateAbaqusInp('81_2phase.inp', MS(sp.rand(81,81,81),2,1,1,1,1))
 
-generateAbaqusInp('41_2phase.inp', MS(sp.rand(41,41,41),2,1,1,1,1))
+generateAbaqusInp('21_2phase_strain.inp', MS(sp.rand(21,21,21),2,1,1,1,1))
+generateAbaqusInp('41_2phase_strain.inp', MS(sp.rand(41,41,41),2,1,1,1,1))
 #generateAbaqusInp('63_2phase.inp', MS(sp.rand(63,63,63),2,1,1,1,1))
 
 #generateAbaqusInp('101_2phase.inp', MS(sp.rand(101,101,101),2,1,1,1,1))
@@ -33,30 +44,26 @@ def generateDeltaM(sideLen, viscoelastic=False):
     middle = sideLen//2
     deltaM_1surroundedBy2[middle, middle, middle] = 1
     if (viscoelastic):
-        generateAbaqusInp('{}deltaM_1surroundedBy2.inp'.format(sideLen), deltaM_1surroundedBy2)
+        generateAbaqusInp('{}deltaM_1surroundedBy2_viscoelastic.inp'.format(sideLen), deltaM_1surroundedBy2)
     else:
-        generateAbaqusInp('{}deltaM_1surroundedBy2_viscoelastic.inp'.format(sideLen), deltaM_1surroundedBy2,True)
+        generateAbaqusInp('{}deltaM_1surroundedBy2.inp'.format(sideLen), deltaM_1surroundedBy2,True)
     deltaM_2surroundedBy1 = sp.ones((sideLen, sideLen, sideLen))
     deltaM_2surroundedBy1[middle, middle, middle] = 2
     if (viscoelastic):
-        generateAbaqusInp('{}deltaM_2surroundedBy1.inp'.format(sideLen), deltaM_2surroundedBy1)
+        generateAbaqusInp('{}deltaM_2surroundedBy1_viscoelastic.inp'.format(sideLen), deltaM_2surroundedBy1)
     else:
-        generateAbaqusInp('{}deltaM_2surroundedBy1_viscoelastic.inp'.format(sideLen), deltaM_1surroundedBy2,True)
+        generateAbaqusInp('{}deltaM_2surroundedBy1.inp'.format(sideLen), deltaM_1surroundedBy2,True)
     
 generateDeltaM(17)
 generateDeltaM(21)
 generateDeltaM(23)
 
+generateDeltaM(15,True)
 generateDeltaM(17,True)
 generateDeltaM(21,True)
 generateDeltaM(23,True)
 
-generateAbaqusInp('21_2phase_viscoelastic.inp', MS(sp.rand(31,31,31),2,1,1,1,1), True)
+generateAbaqusInp('15_2phase_viscoelastic.inp', MS(sp.rand(21,21,21),2,1,1,1,1), True)
+generateAbaqusInp('21_2phase_viscoelastic.inp', MS(sp.rand(21,21,21),2,1,1,1,1), True)
 generateAbaqusInp('31_2phase_viscoelastic.inp', MS(sp.rand(31,31,31),2,1,1,1,1), True)
-generateAbaqusInp('41_2phase_viscoelastic.inp', MS(sp.rand(41,41,41),2,1,1,1,1), True)
-
-f = open('41_2phase.inp.microstructure')
-ms = pickle.load(f)
-f.close()
-
-print ms
+#generateAbaqusInp('41_2phase_viscoelastic.inp', MS(sp.rand(41,41,41),2,1,1,1,1), True)
