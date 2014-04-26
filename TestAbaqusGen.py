f1104846097c662e1b2fdda7c3aa52c5fe8502ae
Jim Allers <jallers@liveperson.com>
2014-04-26 17:04:52 -0400

    Changed viscoelastic simulation to step function for strain

--- TestAbaqusGen.py
+++ TestAbaqusGen.py
@@ -7,24 +7,6 @@ from AbaqusGen import generateAbaqusInp
 from AbaqusGen import getEleNumber
 from AbaqusGen import getIndices
 
-def printElementNumber(i, j, k, side_len):
-    print 'Ele Number for {}, {}, {}: {}'.format(i, j, k, getEleNumber(i, j, k, side_len, side_len, side_len, 1))
-
-printElementNumber(0,0,0,21)
-print getIndices(1, 21)
-printElementNumber(0,20,0,21)
-print getIndices(21, 21)
-printElementNumber(0,20,1,21)
-print getIndices(42, 21)
-printElementNumber(1, 0, 0, 21)
-printElementNumber(0, 0, 1, 21)
-printElementNumber(0, 1, 2, 21)
-print getIndices(44, 21)
-printElementNumber(1, 1, 1, 21)
-printElementNumber(20, 20, 20, 21)
-printElementNumber(11, 11, 11, 21)
-printElementNumber(10, 10, 10, 21)
-
 ms = MS(sp.rand(31,31,31),2,1,1,1,1)
 
 generateAbaqusInp('23_2phase.inp', MS(sp.rand(23,23,23),2,1,1,1,1))
@@ -35,7 +17,8 @@ generateAbaqusInp('17_2phase.inp', MS(sp.rand(17,17,17),2,1,1,1,1))
 
 generateAbaqusInp('21_2phase_strain.inp', MS(sp.rand(21,21,21),2,1,1,1,1))
 generateAbaqusInp('41_2phase_strain.inp', MS(sp.rand(41,41,41),2,1,1,1,1))
-#generateAbaqusInp('63_2phase.inp', MS(sp.rand(63,63,63),2,1,1,1,1))
+generateAbaqusInp('42_2phase_strain.inp', MS(sp.rand(42,42,42),2,1,1,1,1))
+generateAbaqusInp('63_2phase.inp', MS(sp.rand(63,63,63),2,1,1,1,1))
 
 #generateAbaqusInp('101_2phase.inp', MS(sp.rand(101,101,101),2,1,1,1,1))
 
@@ -44,26 +27,28 @@ def generateDeltaM(sideLen, viscoelastic=False):
     middle = sideLen//2
     deltaM_1surroundedBy2[middle, middle, middle] = 1
     if (viscoelastic):
-        generateAbaqusInp('{}deltaM_1surroundedBy2_viscoelastic.inp'.format(sideLen), deltaM_1surroundedBy2)
+        generateAbaqusInp('{}deltaM_1surroundedBy2_viscoelastic.inp'.format(sideLen), deltaM_1surroundedBy2, True)
     else:
         generateAbaqusInp('{}deltaM_1surroundedBy2.inp'.format(sideLen), deltaM_1surroundedBy2,True)
     deltaM_2surroundedBy1 = sp.ones((sideLen, sideLen, sideLen))
     deltaM_2surroundedBy1[middle, middle, middle] = 2
     if (viscoelastic):
-        generateAbaqusInp('{}deltaM_2surroundedBy1_viscoelastic.inp'.format(sideLen), deltaM_2surroundedBy1)
+        generateAbaqusInp('{}deltaM_2surroundedBy1_viscoelastic.inp'.format(sideLen), deltaM_2surroundedBy1, True)
     else:
-        generateAbaqusInp('{}deltaM_2surroundedBy1.inp'.format(sideLen), deltaM_1surroundedBy2,True)
+        generateAbaqusInp('{}deltaM_2surroundedBy1.inp'.format(sideLen), deltaM_1surroundedBy2)
     
 generateDeltaM(17)
 generateDeltaM(21)
 generateDeltaM(23)
+
+generateDeltaM(41)
 
 generateDeltaM(15,True)
 generateDeltaM(17,True)
 generateDeltaM(21,True)
 generateDeltaM(23,True)
 
-generateAbaqusInp('15_2phase_viscoelastic.inp', MS(sp.rand(21,21,21),2,1,1,1,1), True)
+generateAbaqusInp('15_2phase_viscoelastic.inp', MS(sp.rand(15,15,15),2,1,1,1,1), True)
 generateAbaqusInp('21_2phase_viscoelastic.inp', MS(sp.rand(21,21,21),2,1,1,1,1), True)
 generateAbaqusInp('31_2phase_viscoelastic.inp', MS(sp.rand(31,31,31),2,1,1,1,1), True)
 #generateAbaqusInp('41_2phase_viscoelastic.inp', MS(sp.rand(41,41,41),2,1,1,1,1), True)
