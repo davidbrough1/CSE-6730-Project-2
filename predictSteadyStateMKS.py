473595ee410cb8517fd10b82840f8dfdd493b4d5
David Brough <david.brough.0416@gmail.com>
2014-04-28 22:54:07 -0400

    Clean up files

--- predictSteadyStateMKS.py
+++ predictSteadyStateMKS.py
@@ -5,95 +5,87 @@ import pickle as pk
 from MSf import MSf
 import time
 
-
-#print MKS.GenDelta(5,False)
-
+#Generate delta microstructures
 MSf1 = MKS.GenDelta(21,False);
 MSf2 = MKS.GenDelta(21,True);
 
-#filename1 = "deltaM_modeling/21_1_noah2.dat"
-#filename2 = "deltaM_modeling/21_2_noah2.dat"
+#Choose size of prediction microstructure
+predictSize = 21 #63, 42 or 21
 
-filename1 = "deltaM_modeling/21deltaM_1surroundedBy2_smallerstrain.dat"
-filename2 = "deltaM_modeling/21deltaM_2surroundedBy1_smallerstrain.dat"
-filenamePredict = "outputModeling/21_2phase_smallerstrain.dat"
+#Pull in datasets
+filename1 = "calibrationModeling/21deltaM_1surroundedBy2_strain.dat"
+filename2 = "calibrationModeling/21deltaM_2surroundedBy1_strain.dat"
 
-f = open("outputModeling/21_2phase_smallerstrain.inp.microstructure")
+if predictSize == 63:
+    #63 prediction
+    filenamePredict = "validationModeling/63_2phase.dat"
+    f = open("validationModeling/63_2phase.inp.microstructure")
 
-MS41 = pk.load(f)
-MS41[MS41 <= 0] = 1
+elif predictSize == 42:
+    #42 Prediction
+    filenamePredict = "validationModeling/42_2phase_strain.dat"
+    f = open("validationModeling/42_2phase_strain.inp.microstructure")
+
+elif predictSize == 41:
+    #41 Prediction
+    filenamePredict = "validationModeling/41_2phase.dat"
+    f = open("validationModeling/41_2phase.inp.microstructure")    
+
+elif predictSize == 21:
+    #21 Prediction
+    filenamePredict = "validationModeling/21_2phase_strain.dat"
+    f = open("validationModeling/21_2phase_strain.inp.microstructure")
+    
+MSpredict = pk.load(f)
+MSpredict[MSpredict <1] = 2
 
 f.close()
 
+#Extract strain values
 strain1 = MKS.ABstrains(filename1)
 strain2 = MKS.ABstrains(filename2)
-strain41 = MKS.ABstrains(filenamePredict)
+strainpredict = MKS.ABstrains(filenamePredict)
+strainpredict = strainpredict/np.mean(strainpredict)
 
-MSf41 = MSf(MS41)
-MSf41 = MSf41[:,:,:,0,:]
-
-#slice = stress2[:,:,8]
-#MKS.ShowSlice(slice, slice.min(), slice.max())
+#Generate Microstructure Function
+MSfpredict = MSf(MSpredict)
+MSfpredict = MSfpredict[:,:,:,0,:]
 
 Macro = .02
-
-
 coeff = MKS.GenC(MSf1, MSf2, filename1, filename2, Macro)
 
-'''
-plt.imshow(np.real_if_close(np.fft.ifftn(coeff[10,:,:,0])))
-plt.colorbar()
-plt.show()
-'''
-'''
-Coeff0 = np.real_if_close(np.fft.ifftn(coeff[:,:,:,0]))
-Coeff0 = np.roll(Coeff0,10,axis = 0)
-Coeff0 = np.roll(Coeff0,10,axis = 1)
-Coeff0 = np.roll(Coeff0,10,axis = 2)
-coeff41 = MKS.ExpandCoeff(coeff,MSf41.shape[0])
-plt.subplot(311)     
-plt.imshow(np.real_if_close(Coeff0[10,:,:]))
-plt.colorbar()
-plt.title('Calibrated Coefficients')
-Coeff410 = np.fft.ifftn(coeff41[:,:,:,0])
-Coeff411 = np.fft.ifftn(coeff41[:,:,:,1])
-plt.subplot(312)
-plt.imshow(np.real_if_close(Coeff410[10,0:21,0:21]))
-plt.colorbar()
-plt.title('Expanded Coefficients in Small Window')
-plt.subplot(313)
-plt.imshow(np.real_if_close(Coeff0[10,:,:]-Coeff410[10,0:21,0:21]))
-plt.colorbar()
-plt.title('Difference between 2 above')
-plt.show()
-'''
+#Number of slice to look at.
+n= 13
+
+if MSpredict.shape[0] > coeff.shape[0]:
+    coeff = MKS.ExpandCoeff(coeff,MSpredict.shape[0])
+    n = MSpredict.shape[0]/2+3
                         
 startTime = time.time()
-strainCalc41 = np.real_if_close(MKS.NewResponse(coeff, Macro, MSf41))
+strainCalcpredict = np.real_if_close(MKS.NewResponse(coeff, Macro, MSfpredict))
 endTime = time.time()
-print endTime-startTime
+print 'Time to compute new response',(endTime-startTime),'secs'
 
-plt.subplot(311)
-p1 = plt.imshow(strainCalc41[10,:,:])
+plt.subplot(211)
+p1 = plt.imshow(strainCalcpredict[n,:,:]/np.mean(strainCalcpredict))
 plt.colorbar()
-plt.title('MKS')
-plt.subplot(312)
-p2 = plt.imshow(strain41[10,:,:])
+plt.title('Results using MKS')
+plt.subplot(212)
+p2 = plt.imshow(strainpredict[n,:,:])
 plt.colorbar()
-plt.title('FEM')
-plt.subplot(313)
-p3 = plt.imshow(MS41[10,:,:])
+plt.title('Results using FEM')
+plt.show()
+plt.subplot(211)
+p3 = plt.imshow(MSpredict[n,:,:])
+plt.title('2 Phase Microstructure')
+plt.subplot(212)
+#error = ((strainCalcpredict[n,:,:])/np.mean(strainCalcpredict)-strainpredict[n,:,:])\
+        #/(strainCalcpredict[n,:,:]/np.mean(strainCalcpredict))
+p3 = plt.imshow(((strainCalcpredict[n,:,:]/np.mean(strainCalcpredict))-strainpredict[n,:,:])\
+        /(strainCalcpredict[n,:,:]/np.mean(strainCalcpredict)))
+plt.title('Error between FEM and MKS')
 plt.colorbar()
 plt.show()
-plt.title('Microstructure')
-plt.close()
 
+print 
 
-
-
-'''
-plt.imshow(strainCalc41[10,:,:])
-plt.show()
-plt.imshow(MS41[10,:,:])
-plt.show()
-'''
